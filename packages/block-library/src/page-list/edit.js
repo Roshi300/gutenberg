36dbf9afc23d7e2107c7ac981e4a81e3560eb26b
MaggieCabrera <maggie.cabrera@automattic.com>
2023-03-06 12:03:32 +0100

    added translators note

--- packages/block-library/src/page-list/edit.js
+++ packages/block-library/src/page-list/edit.js
@@ -221,6 +221,7 @@ export default function PageListEdit( {
 			const pageProps = {
 				id: page.id,
 				label:
+					// translators: displayed when a page has an empty title.
 					page.title?.rendered !== ''
 						? page.title?.rendered
 						: __( '(No title)' ),
